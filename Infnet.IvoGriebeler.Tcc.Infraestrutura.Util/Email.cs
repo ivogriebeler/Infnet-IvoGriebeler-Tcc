1f437713fa4429a34cd0a16ef6e7c7096c9665b5
ivogriebeler <ivogriebeler@yahoo.com.br>
2015-09-14 00:29:06 -0300

    Criação do método para envio de e-mail.

--- Infnet.IvoGriebeler.Tcc.Infraestrutura.Util/Email.cs
+++ Infnet.IvoGriebeler.Tcc.Infraestrutura.Util/Email.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,61 @@ namespace Infnet.IvoGriebeler.Tcc.Infraestrutura.Util
 {
     public static class Email
     {
-        public static void Enviar()
+        public static RetornoEmail Enviar(string emailDestino, string assuntoEmail, string corpoEmail)
         {
-            throw new NotImplementedException();
+            var userName = "criar e-mail padrão";
+            var senha = "qweQwe123@#$";
+
+            // Link para habilitar no Gmail
+            // https://www.google.com/settings/security/lesssecureapps
+
+            try
+            {
+                using (SmtpClient cliente = new SmtpClient())
+                {
+                    cliente.Host = "smtp.gmail.com";
+                    cliente.EnableSsl = true;
+                    cliente.Port = 587; // se não der, testar 465
+                    cliente.UseDefaultCredentials = false;
+                    cliente.Credentials = new NetworkCredential(userName, senha);
+
+                    using (MailMessage mensagem = new MailMessage())
+                    {
+                        mensagem.From = new MailAddress(userName, "Definir");
+                        mensagem.To.Add(emailDestino);
+                        mensagem.Subject = assuntoEmail;
+                        mensagem.Body = corpoEmail;
+                        mensagem.SubjectEncoding = Encoding.GetEncoding("ISO-8859-1");
+                        mensagem.BodyEncoding = Encoding.GetEncoding("ISO-8859-1");
+
+                        cliente.Send(mensagem);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return new RetornoEmail(ex);
+            }
+
+            return new RetornoEmail();
+        }
+    }
+
+    public class RetornoEmail
+    {
+        public bool Sucesso { get; private set; }
+
+        public Exception Excecao { get; private set; }
+
+        public RetornoEmail()
+        {
+            Sucesso = true;
+        }
+
+        public RetornoEmail(Exception excpetion)
+        {
+            Sucesso = false;
+            Excecao = excpetion;
         }
     }
 }
