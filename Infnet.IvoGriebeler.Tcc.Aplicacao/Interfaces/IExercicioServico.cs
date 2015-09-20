5fa9d8782a9aa8c75935c7ca80d63cfb25d5a906
Ivo <ivo@hgm.com.br>
2015-09-20 20:53:05 -0300

    Exercícios parcial.

--- Infnet.IvoGriebeler.Tcc.Aplicacao/Interfaces/IExercicioServico.cs
+++ Infnet.IvoGriebeler.Tcc.Aplicacao/Interfaces/IExercicioServico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infnet.IvoGriebeler.Tcc.Aplicacao.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Infnet.IvoGriebeler.Tcc.Aplicacao.Interfaces
 {
     public interface IExercicioServico
     {
+        IList<ExercicioDto> ObterTodos();
+        ExercicioDto ObterPorId(Guid id);
+        ExercicioDto Adicionar(ExercicioDto exercicio);
+        ExercicioDto Atualizar(ExercicioDto exercicio);
+        void Excluir(ExercicioDto exercicio);
     }
 }
---
+++ Infnet.IvoGriebeler.Tcc.Mvc/Areas/Administracao/Models/ExercicioModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Infnet.IvoGriebeler.Tcc.Mvc.Areas.Administracao.Models
+{
+    public class ExercicioModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        [Required]
+        [Display(Name = "Duração")]
+        public int Duracao { get; set; }
+    }
+}
